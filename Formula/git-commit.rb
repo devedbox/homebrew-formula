c62f42045c989f93183cb1efecae557d9e8b68a4
devedbox <devedbox@gmail.com>
2018-08-07 16:23:37 +0800

    feat: Adds formula.

---
+++ Formula/git-commit.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class GitCommit < Formula
+  desc "A tool to check commit message contents of git."
+  version '0.0.1'
+  homepage "https://github.com/devedbox/git-commit"
+  url "https://github.com/devedbox/git-commit.git",
+    :tag => '0.0.1'
+  head "https://github.com/devedbox/git-commit.git"
+
+  depends_on :xcode => "8.0"
+  depends_on :xcode => ["9.0", :build]
+
+  def install
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test git-commit`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
